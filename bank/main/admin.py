75e6f5ea8fca403ccd4df6dd6364932b651ee834
Varun Mamtora <varunmamtora@gmail.com>
2021-09-22 20:17:44 +0530

    register models in admin

--- bank/main/admin.py
+++ bank/main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+@admin.register(CustomerProfile)
+class CustomerProfileAdmin(admin.ModelAdmin):
+    list_display=['id','prof_for','phone','email']
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display=['id','acc_no','acc_for','acc_type', 'acc_bal']
