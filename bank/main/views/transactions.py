833781836b2eb2b324a62f925ee14b0a69f4b490
Varun Mamtora <varunmamtora@gmail.com>
2021-09-24 00:15:39 +0530

    return tranctions

--- bank/main/views/transactions.py
+++ bank/main/views/transactions.py
@@ -13,13 +13,14 @@ def index(request):
 
 @csrf_exempt
 def get_transactions(request):
-    # user = request.user
-    user = User.objects.get(username="Shyren@473793737284")
-    if request.method == 'POST':
-        user_account = Account.objects.get(acc_for=CustomerProfile.objects.get(prof_for=user))
-        transactions = Transaction.objects.filter(Q(sender=user_account) | Q(receiver=user_account))
-        for transaction in transactions:
-            if transaction.sender == user_account:
-                print(f"red {transaction.amount}")
-            else:
-                print(f"green {transaction.amount}")
+    user = request.user
+    # user = User.objects.get(username="Shyren@473793737284")
+    user_account = Account.objects.get(acc_for=CustomerProfile.objects.get(prof_for=user))
+    transactions = Transaction.objects.filter(Q(sender=user_account) | Q(receiver=user_account))
+    # for transaction in transactions:
+    #     if transaction.sender == user_account:
+    #         print(f"red {transaction.amount}")
+    #     else:
+    #         print(f"green {transaction.amount}")
+    
+    return render(request, 'account_activity.html', {"transactions":transactions})
