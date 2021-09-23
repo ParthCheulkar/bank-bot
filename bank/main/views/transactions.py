c1cc1fbb003bfabf2745ec7b03235dabf535eead
Varun Mamtora <varunmamtora@gmail.com>
2021-09-24 02:21:35 +0530

    add transfer money

--- bank/main/views/transactions.py
+++ bank/main/views/transactions.py
@@ -3,7 +3,8 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse 
 from ..models import * 
 from django.views.decorators.csrf import csrf_exempt
-
+from django.contrib import messages
+from decimal import Decimal
 
 
 def index(request):
@@ -24,3 +25,40 @@ def get_transactions(request):
     #         print(f"green {transaction.amount}")
     
     return render(request, 'account_activity.html', {"transactions":transactions, "user_account":user_account})
+
+def make_transaction(request):
+    user = request.user
+
+    user_account = Account.objects.get(acc_for=CustomerProfile.objects.get(prof_for=user))
+
+    if request.method == "POST":
+        rname = request.POST["rname"]
+        accountno = request.POST["accountno"]
+        amount = Decimal(request.POST["amount"])
+        remark = request.POST["remark"]
+
+        receiver = Account.objects.get(acc_no = accountno)
+        print(f"reci->{receiver.acc_no}")
+        print(f"sender->{user_account.acc_no}")
+        if user_account.acc_bal > amount+Decimal(100.0):
+            try:
+                print(f"moneyy->{user_account.acc_bal}")
+                receiver.acc_bal += amount
+                user_account.acc_bal -= amount
+
+                receiver.save()
+                user_account.save()
+
+                Transaction.objects.create(transaction_type = 'IMPS', sender = user_account, receiver = receiver, amount = amount, status = 'successful', remark=remark)
+
+
+                messages.success(request, "Transaction success.")
+                return render(request, "transfer_money.html")
+            except:
+                messages.error(request, "Transaction failed.")
+                return render(request, "transfer_money.html")
+        else:
+            messages.error(request, "You dont have enough balance.")
+            return render(request, "transfer_money.html")
+    else:
+        return render(request, "transfer_money.html")
