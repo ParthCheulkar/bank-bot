ce0f762d3bb8c9d8848c1db5b08a9776f63326d3
Chiraayu_Pravin <chiraayupm@gmail.com>
2021-09-23 12:43:33 +0530

    chatroom ui and bot

--- bank/main/urls.py
+++ bank/main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import authentication, main
+from .views import authentication, main, chatroom
 
 urlpatterns = [
     path('', main.index, name="index"),
     path('login/', authentication.login, name="login"),
+    path('chatroom/', chatroom.chatroom, name="chatroom"),
 ]
